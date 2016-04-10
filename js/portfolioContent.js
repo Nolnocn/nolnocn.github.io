643b1ba72bb3d161646598b168c642e8219e080b
Nick Conlon <ncon67@gmail.com>
2016-04-10 00:56:32 -0400

    Added content for TaT

--- js/portfolioContent.js
+++ js/portfolioContent.js
@@ -6,7 +6,7 @@ site.portfolioContent = {
         img: "img/projects/tat.png",
         git: "https://github.com/Nolnocn/Trick-and-Treat",
         demo: "demos/trickandtreat",
-        desc: "<p>Trick and Treat is...</p>"
+        desc: "<p>Trick and Treat was the first of three games I worked on for my Game Design and Development course I took during my third year at RIT. The game was built in five weeks using Unity 4. It is a 2D beat 'em up style game about a delusional \"ghost buster\" who takes to the streets on Halloween to rid the world of ghosts. It is a very silly game that I had a lot of fun working on.</p><p>We had a team of eight people building the game, which was the largest team I had ever worked with up to that point. I was primarily the gameplay programmer on the team. I worked on basically anything that wasn't AI or UI. I programmed features such as player movement and actions, as well as systems such as the infinitely scrolling level and parallaxed background.</p><p>Trick and Treat holds a special place in my heart as one of my favorite projects I've ever worked on. Firstly, because it was the first time I had worked with a team on a game in Unity, after about three years of messing around with the engine on my own. And things went great! The team worked very well together and we had so many people that each team member got to specialize in something that interested them. We had four programmers, three artists, and even a sound designer which was another first for a project I worked on.</p><p>Though, one of the things I am most proud of about Trick and Treat is that the game was my idea. For the course, each person in the class had to give a presentation and pitch a game idea. As an added design challenge, the game had to involve a vacuum cleaner somehow. I never really considered myself much of a design guy, but I thought of a funny idea and decided to roll with it. After the presentations, everyone voted on the games they wanted to work on and seven other people ended up working on this game with me.</p>"
     },
     
     sbl: {
@@ -14,7 +14,7 @@ site.portfolioContent = {
         img: "img/projects/sbl.png",
         git: "https://github.com/Nolnocn/Southern-But-Love",
         demo: "demos/southernbutlove",
-        desc: "<p>Southern, But Love is...</p>"
+        desc: ""
     },
     
     thepatrol: {
@@ -49,11 +49,3 @@ site.portfolioContent = {
         desc: "<p>The Animaker is...</p>"
     }
 };
-
-/*
-        this.modalTitle = $( "#modalTitle" );
-        this.modalImg = $( "#modalImg" );
-        this.modalGithub = $( "#modalGithub" );
-        this.modalDemo = $( "#modalDemo" );
-        this.modalDesc = $( "#modalDesc" );
-*/
